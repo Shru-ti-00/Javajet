6fa830afcdda9ced9ac2b11dcef32c2e2621da76
Shru-ti-00 <shrutimunda@gmail.com>
2026-04-05 23:28:27 +0530

    Added program to print table of any no.

---
+++ table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class table
+{
+    void prnTable(int num)
+    {
+        int i=1;
+        for(int i=1;i<10;i++)
+        {
+            System.out.println(num*i);
+        }
+    }
+}
